83f24595b7447606cf0b81c9250df671c8d7db82
Arindamonlive <109526672+Arindamonlive@users.noreply.github.com>
2025-05-26 00:42:37 +0530

    correct the amount

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -177,7 +177,7 @@ const Course = () => {
             onClick={() =>
               setSelectedCourse({
                 title: "Final Year Project Assistance",
-                fee: 1999,
+                fee: 2999,
               })
             }
             className="bg-indigo-600 hover:bg-indigo-700 text-white px-4 py-2 rounded-lg font-medium transition"
